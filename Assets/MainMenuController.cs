2206eb0ba301ba36f71c28dfd4a24b448eb481e3
thebatoust <thebatoust@gmail.com>
2016-01-30 09:37:13 +0100

    Added DOTween + Main menu tweening

---
+++ Assets/MainMenuController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class MainMenuController : MonoBehaviour {
+
+    public GameObject titleGameObject;
+
+    void Awake()
+    {
+        DOTween.Init();
+    }
+
+	// Use this for initialization
+	void Start () {
+        titleGameObject.transform.DOShakeScale(3.0f, 1.0f, 6);
+        titleGameObject.transform.DOShakeRotation(3.0f, 30.0f, 6);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
