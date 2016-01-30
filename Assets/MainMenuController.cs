300725e31c1e9b4fff7944c88e5c92cf49ddc692
thebatoust <thebatoust@gmail.com>
2016-01-30 10:27:58 +0100

    Added main menu tweening and animations

--- Assets/MainMenuController.cs
+++ Assets/MainMenuController.cs
@@ -1,24 +1,60 @@
 ﻿using UnityEngine;
-using UnityEngine.UI;
 using DG.Tweening;
+using UnityEngine.UI;
 
 public class MainMenuController : MonoBehaviour {
 
     public GameObject titleGameObject;
 
+    public Button playButton, quitButton;
+
     void Awake()
     {
         DOTween.Init();
     }
-
-	// Use this for initialization
+    
 	void Start () {
+        DoInitialTween();
+	}
+
+    ////////////////////////////////////////////////////////
+    // Button events                                    ////
+    ////////////////////////////////////////////////////////
+
+    public void LaunchNewGame()
+    {
+        // ^
+        // |
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    ////////////////////////////////////////////////////////
+    // GFX / Tweening / Animations / SFX                ////
+    ////////////////////////////////////////////////////////
+
+    void DoInitialTween()
+    {
+        // Title shake !!
         titleGameObject.transform.DOShakeScale(3.0f, 1.0f, 6);
         titleGameObject.transform.DOShakeRotation(3.0f, 30.0f, 6);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    }
+
+    public void AnimateOnHoverButton(Button button)
+    {
+        Sequence sequence = DOTween.Sequence();
+        sequence.Append(button.transform.DOScale(1.1f, 0.8f));
+        sequence.PrependInterval(0.4f);
+        sequence.Insert(0, button.transform.DOShakeScale(0.4f, 0.3f, 2));
+
+        sequence.Play();
+    }
+
+    public void AnimateOnHoverExitButton(Button button)
+    {
+        button.transform.DOScale(1.0f, 0.2f);
+    }
 }
